85aa38b3db38e919da2105e31b7e9f19af495c7e
AMIRTHAM <155797726+AMIRTHAM23@users.noreply.github.com>
2026-03-02 15:21:14 +0530

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-
-const lights = document.querySelectorAll('.light');
-function turnOn(color) {
-    lights.forEach(light => light.classList.remove('on'));
-    document.querySelector('.' + color).classList.add('on');
-}
