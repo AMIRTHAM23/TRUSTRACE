6dd1a87d547ca555eb86e275dfde3736c7c6b785
AMIRTHAM23 <amirtham2309@gmail.com>
2026-03-03 10:32:29 +0530

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,12 @@ import Login from './components/Login';
 
 function App() {
   return (
-    <h1>Job Portal App</h1>
+    <Router>
+      <Routes>
+        <Route path="/register" element={<Registration />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </Router>
   );
 }     
 export default App;
